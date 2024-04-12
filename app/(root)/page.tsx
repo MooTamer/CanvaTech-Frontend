91cec63eb972e96245ca31dd059a602c90d68c25
MooTamer <mohamedtamer200200@gmail.com>
2024-04-12 22:36:39 +0200

    homepage spotlight

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
+import React from "react";
+import { cn } from "@/utils/cn";
+import { Spotlight } from "@/components/ui/Spotlight";
+
 export default function Home() {
   return (
-    <>
-      <h1 className="head-text text-left">Home </h1>
-    </>
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-5xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          CanvaTech
+        </h1>
+        <p className="mt-4 font-normal text-3xl text-neutral-300 max-w-lg text-center mx-auto">
+          Designing dreams, one palette at a time.
+        </p>
+      </div>
+    </div>
   );
 }
