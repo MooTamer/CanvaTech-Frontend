b500ff8ccc483a1828decda0d83af8a8add872cc
MooTamer <mohamedtamer200200@gmail.com>
2024-04-12 22:19:05 +0200

    index

---
+++ lib/validations/user.ts
@@ -0,0 +1,8 @@
+import * as z from 'zod';
+
+export const UserValidation = z.object({ 
+    username: z.string().min(3).max(30),
+    name: z.string().min(3).max(30),
+    bio: z.string().min(3).max(1000),
+    profile_photo: z.string().url(),
+    });  
