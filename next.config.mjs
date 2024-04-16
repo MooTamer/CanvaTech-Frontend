c17011b87120c2d58eb2567f4e5423a14fc198bf
MooTamer <mohamedtamer200200@gmail.com>
2024-04-16 06:10:40 +0200

    animated scroll effect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+export default {
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
 
-export default nextConfig;
+//  nextConfig;
