cc277c41773e8ed72d28d130ebfd74ee1183ab3a
mznmo <102366479+mznmo@users.noreply.github.com>
2024-05-14 21:57:40 +0300

    custom palette fixed

--- app/(root)/customPallete/page.tsx
+++ app/(root)/customPallete/page.tsx
@@ -1,11 +1,13 @@
-"use client";
+'use client'
 import React, { useState } from "react";
 import "tailwindcss/tailwind.css";
 
 const CustomPage = () => {
   const colors = ["red", "green", "blue"];
+  const images = ["pallete1.webp", "pallete2.webp", "pallete3.jpg", "pallete4.webp"];
 
   const [selectedColor, setSelectedColor] = useState(colors[0]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const handleColorSelect = (color) => {
     setSelectedColor(color);
@@ -29,165 +31,95 @@ const CustomPage = () => {
     }
   };
 
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
   return (
-    <div className="container mx-auto px-4 sm:px-8 py-8 h-full">
-      <div className="navbar bg-base-100 flex items-center justify-between px-4 py-2 mb-4">
-        <div className="flex items-center">
-          <a href="/products" className="btn btn-ghost text-xl">
-            CanvaTech
-          </a>
-        </div>
-        <div className="hidden sm:flex flex-grow justify-center space-x-4 sm:space-x-8">
-          <a
-            href="/customPallete"
-            className="font-bold text-white hover:text-gray-300"
+    <div className="container mx-auto px-4 sm:px-8 py-16 h-full flex items-center justify-center">
+      <div className="bg-black-100 p-4 sm:p-8 rounded-lg mb-4 sm:mb-0 sm:mr-4">
+        <div className="w-full sm:w-96 rounded-box overflow-hidden relative">
+          <img
+            src={images[currentImageIndex]}
+            className="w-full h-96 object-cover rounded-lg transition-transform duration-300 transform"
+            alt={`Ready To Go Pallets ${currentImageIndex + 1}`}
+          />
+          <button
+            className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2 shadow-md"
+            onClick={handlePrevImage}
           >
-            Customize your own
-          </a>
-          <a href="/rental" className="text-white hover:text-gray-300">
-            Rent
-          </a>
-          <a href="#" className="text-white hover:text-gray-300">
-            Contact
-          </a>
-        </div>
-        <div className="flex items-center">
-          <div className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="badge badge-sm indicator-item">8</span>
-            </div>
-          </div>
-          <div className="dropdown dropdown-end ml-4">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Profile Avatar"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                />
-              </div>
-            </div>
-            <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-40 sm:w-52">
-              <li>
-                <a className="justify-between">Profile</a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a href="/login">Logout</a>
-              </li>
-            </ul>
-          </div>
+            {"<"}
+          </button>
+          <button
+            className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2 shadow-md"
+            onClick={handleNextImage}
+          >
+            {">"}
+          </button>
         </div>
       </div>
-
-      <div className="flex flex-col items-center justify-center sm:flex-row">
-        <div className="bg-black-100 p-4 sm:p-8 rounded-lg mb-4 sm:mb-0 sm:mr-4">
-          <div className="w-full sm:w-96 carousel rounded-box">
-            <div className="carousel-item w-full">
-              <img
-                src="pallete1.webp"
-                className="w-full h-auto rounded-lg"
-                alt="Ready To Go Pallets 1"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="pallete2.webp"
-                className="w-full h-auto rounded-lg"
-                alt="Ready To Go Pallets 2"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="pallete3.jpg"
-                className="w-full h-auto rounded-lg"
-                alt="Ready To Go Pallets 3"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="pallete4.webp"
-                className="w-full h-auto rounded-lg"
-                alt="Ready To Go Pallets 4"
-              />
-            </div>
-          </div>
+      <div className="text-center sm:text-left">
+        <h2 className="text-2xl font-bold mb-2">Customized Pallets</h2>
+        <div className="text-lg font-italic text-gray-500 mb-4">
+          1,600 EGP
         </div>
-        <div className="text-center sm:text-left">
-          <h2 className="text-2xl font-bold mb-2">Customized Pallets</h2>
-          <div className="text-lg font-italic text-gray-500 mb-4">
-            1,600 EGP
-          </div>
-          <div className="text-sm font-bold text-gray-500 mb-4">Dimensions</div>
-          <div className="flex mb-4">
+        <div className="text-sm font-bold text-gray-500 mb-4">Dimensions</div>
+        <div className="flex mb-4">
+          <div className="flex-1 pr-2">
             <input
               type="text"
-              className="input mr-4"
-              style={{ width: "30%" }}
+              className="input rounded-lg border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-500 p-2 w-full"
               placeholder="Width"
             />
+          </div>
+          <div className="flex-1 pr-2">
             <input
               type="text"
-              className="input"
-              style={{ width: "30%" }}
+              className="input rounded-lg border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-500 p-2 w-full"
               placeholder="Height"
             />
           </div>
-          <input
-            type="text"
-            className="input mb-4"
-            style={{ width: "30%" }}
-            placeholder="Length"
-          />
-          <div className="text-sm font-bold text-gray-500 mb-4">Color</div>
-          <div className="flex items-center mb-4">
-            {colors.map((color) => (
-              <div
-                key={color}
-                className={`w-8 h-8 rounded-full cursor-pointer mr-2 ${
-                  selectedColor === color ? `border-2 border-${color}-500` : ""
-                }`}
-                style={{ backgroundColor: color }}
-                onClick={() => handleColorSelect(color)}
-              ></div>
-            ))}
+          <div className="flex-1">
+            <input
+              type="text"
+              className="input rounded-lg border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-500 p-2 w-full"
+              placeholder="Length"
+            />
           </div>
-          <div className="flex items-center justify-center sm:justify-start mb-4">
-            <button className="btn btn-quantity" onClick={handleDecrement}>
-              -
-            </button>
-            <span id="quantity" className="mx-4 text-lg font-semibold">
-              1
-            </span>
-            <button className="btn btn-quantity" onClick={handleIncrement}>
-              +
-            </button>
-          </div>
-          <div className="flex items-center justify-center sm:justify-start">
-            <button className="btn btn-primary mr-4">Buy Now</button>
-            <div className="flex items-center text-gray-500 font-medium mt-2 sm:mt-0">
-              <div className="border-t border-gray-300 w-full"></div>
-              <div className="border-t border-gray-300 w-full"></div>
-            </div>
+        </div>
+        <div className="text-sm font-bold text-gray-500 mb-4">Color</div>
+        <div className="flex items-center mb-4">
+          {colors.map((color) => (
+            <div
+              key={color}
+              className={`w-6 h-6 rounded-full cursor-pointer mr-2 ${
+                selectedColor === color ? `border-2 border-${color}-500` : ""
+              }`}
+              style={{ backgroundColor: color }}
+              onClick={() => handleColorSelect(color)}
+            ></div>
+          ))}
+        </div>
+        <div className="flex items-center justify-center sm:justify-start mb-4">
+          <button className="btn btn-quantity bg-blue-500 text-white rounded-full py-2 px-4" onClick={handleDecrement}>
+            -
+          </button>
+          <span id="quantity" className="mx-2 text-lg font-semibold">
+            1
+          </span>
+          <button className="btn btn-quantity bg-blue-500 text-white rounded-full py-2 px-4" onClick={handleIncrement}>
+            +
+          </button>
+        </div>
+        <div className="flex items-center justify-center sm:justify-start">
+          <button className="btn btn-primary bg-blue-500 text-white rounded-full py-2 px-4 mr-4">Buy Now</button>
+          <div className="flex items-center text-gray-500 font-medium mt-2 sm:mt-0">
+            <div className="border-t border-gray-300 w-full"></div>
+            <div className="border-t border-gray-300 w-full"></div>
           </div>
         </div>
       </div>
